96c2b6f86dff238c68bd35095442fa609e30ab46
anabil97 <38479039+anabil97@users.noreply.github.com>
2023-08-25 11:43:15 +0300

    5th push

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
 provider "aws" {
 
     region  = "eu-central-1"
-    assume_role {
-    role_arn       = "arn:aws:iam::574271174528:role/ECR-Role"
-    session_name   = "account-session"
-    }
+    # assume_role {
+    # role_arn       = "arn:aws:iam::574271174528:role/ECR-Role"
+    # session_name   = "account-session"
+    # }
 
 }
